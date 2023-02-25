66fa6c42f134f9edf0e427b43b427ff28b45ba16
SavaStyle <sava.90@inbox.ru>
2023-02-25 22:50:58 +0800

    add-item-requests11

---
+++ src/main/java/ru/practicum/shareit/request/RequestMapper.java
@@ -0,0 +1,25 @@
+package ru.practicum.shareIt.request;
+
+public class RequestMapper {
+
+    public static Request fromRequestDto(RequestDto requestDto) {
+        return new Request(requestDto.getId(),
+                requestDto.getDescription(),
+                requestDto.getRequestor(),
+                requestDto.getCreated());
+    }
+
+    public static RequestDtoResponse toRequestDto(Request request) {
+        return new RequestDtoResponse(request.getId(),
+                request.getDescription(),
+                request.getRequestor(),
+                request.getCreated());
+    }
+
+    public static RequestDtoResponse toRequestDtoTest(RequestDto requestDto) {
+        return new RequestDtoResponse(requestDto.getId(),
+                requestDto.getDescription(),
+                requestDto.getRequestor(),
+                requestDto.getCreated());
+    }
+}
